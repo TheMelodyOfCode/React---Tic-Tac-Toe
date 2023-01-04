895bd5feec5249483cb66f531a59b81ca9e60087
D.Cameron <lobbygow@posteo.de>
2023-01-04 16:51:46 +0100

    restructured app

--- src/components/game/game.component.jsx
+++ src/components/game/game.component.jsx
@@ -1,5 +1,7 @@
 import * as React from 'react'
+
 import { useLocalStorageState} from '../../utils/syncLocalStorage'
+import { calculateStatus, calculateNextValue, calculateWinner } from '../../utils/calculations'
 
 import Board from '../board/board.component'
 
@@ -79,35 +81,35 @@ function selectSquare(square) {
 export default Game
 
 
-function calculateStatus(winner, squares, nextValue) {
-    return winner
-      ? `Winner: ${winner}`
-      : squares.every(Boolean)
-      ? `Scratch: Cat's game`
-      : `Next player: ${nextValue}`
-  }
+// function calculateStatus(winner, squares, nextValue) {
+//     return winner
+//       ? `Winner: ${winner}`
+//       : squares.every(Boolean)
+//       ? `Scratch: Cat's game`
+//       : `Next player: ${nextValue}`
+//   }
   
-  function calculateNextValue(squares) {
-    return squares.filter(Boolean).length % 2 === 0 ? 'X' : 'O'
-  }
+//   function calculateNextValue(squares) {
+//     return squares.filter(Boolean).length % 2 === 0 ? 'X' : 'O'
+//   }
   
 
-  function calculateWinner(squares) {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ]
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i]
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a]
-      }
-    }
-    return null
-  }
+//   function calculateWinner(squares) {
+//     const lines = [
+//       [0, 1, 2],
+//       [3, 4, 5],
+//       [6, 7, 8],
+//       [0, 3, 6],
+//       [1, 4, 7],
+//       [2, 5, 8],
+//       [0, 4, 8],
+//       [2, 4, 6],
+//     ]
+//     for (let i = 0; i < lines.length; i++) {
+//       const [a, b, c] = lines[i]
+//       if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+//         return squares[a]
+//       }
+//     }
+//     return null
+//   }
